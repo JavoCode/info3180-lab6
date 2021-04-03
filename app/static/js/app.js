7869522a9cc6cee854ce4d8184828c35298bbbba
Javon Ellis <javon.ellis@aye-tech.com>
2021-04-03 16:20:34 -0500

    final update

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,10 +1,116 @@
+
+
+const Home = {
+    name:'Home',
+    template: `
+    <div class="home">
+ <img src="/static/images/logo.png" alt="VueJS Logo">
+ <h1>{{ welcome }}</h1>
+ </div>`,
+    data(){
+        return {
+            welcome:'Hello'
+        }
+    }
+}
+
+const NewsList = {
+  name:'news-list',
+  template: `
+    <div class=" justify-content-center"  >
+    <div class="form-inline d-flex justify-content-center">
+        <div class="form-group mx-sm-3 mb-2">
+        <label class="sr-only" for="search">Search</label>
+        <input type="search" name="search" v-model="searchTerm"
+            id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+            <div class="button-grid">
+            <button class="btn btn-primary mb-2" @click="searchNews" >Search</button>
+        <button class=" btn btn-primary mb-2" @click="topStories" >Top Stories</button>
+            </div>
+       
+        </div>
+    </div>
+     <h2 class=" d-flex justify-content-center">News</h2>
+     <div class="card-grid ">
+             <div class="card " style="width: 18rem;"  v-for="article in articles">
+            <img class="card-img-top" :src="article.urlToImage" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">{{ article.title }}</h5>
+                    <p class="card-text">{{article.description}}</p>
+                </div>
+        </div>
+     </div>
+
+    </div>
+  `,
+    data: function() {
+      return {
+         articles:[],
+          searchTerm: ''
+      }
+  },
+  created() {
+
+      this.topStories();
+
+
+  },
+    methods : {
+        searchNews() {
+         let self = this;
+         if(self.searchTerm == ''){
+             self.topStories()
+         }else {
+
+             fetch('https://newsapi.org/v2/everything?q='+
+            self.searchTerm + '&language=en', {
+             headers: {
+             'Authorization': 'Bearer 405e1cdb82fa4bf38a6362f88e6a4550'
+             }
+         })
+             .then(function(response) {
+             return response.json();
+         })
+             .then(function(data) {
+         console.log(data);
+         self.articles = data.articles;
+         });
+         }
+
+     },
+        topStories() {
+           //vue instance access
+    let self = this;
+
+    fetch('https://newsapi.org/v2/top-headlines?country=us' ,
+        {
+          headers: {
+            'Authorization':'Bearer 405e1cdb82fa4bf38a6362f88e6a4550'
+          }
+        }).then(function (response){
+          return response.json();
+        }).then(function (data) {
+          console.log(data)
+          self.articles = data.articles
+
+        }
+        )
+        }
+    },
+}
+
+
 /* Add your Application JavaScript */
 const app = Vue.createApp({
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
     }
-  }
+  },
+    components : {
+      'home': Home,
+        'news-list':NewsList
+    }
 });
 
 app.component('app-header', {
@@ -20,10 +126,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  <router-link to="/" class="nav-link"> Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                  <router-link to="/news" class="nav-link"> News</router-link>
                 </li>
               </ul>
             </div>
@@ -51,4 +157,18 @@ app.component('app-footer', {
   }
 })
 
+
+
+
+const router = VueRouter.createRouter( {
+    history: VueRouter.createWebHistory(),
+    routes:[
+        {path:'/',component:Home},
+        {path:'/news', component: NewsList}
+    ]
+})
+
+
+
+app.use(router)
 app.mount('#app');
